b3da54b608426454cfef5254668398ac8581a09b
royhung <lemanhhung1555@gmail.com>
2025-12-25 23:37:39 +0700

    Alert (extra)

---
+++ chapter06/video54/video54.js
@@ -0,0 +1,8 @@
+console.log("Video 54");
+
+const element = document.getElementById("login");
+
+element.addEventListener("click", () => {
+  console.log("click me");
+  alert("Login khong thanh cong!");
+});
