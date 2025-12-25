4c5183ba544a1107e6987bbc425a330d14606816
royhung <lemanhhung1555@gmail.com>
2025-12-25 22:10:10 +0700

    Lăng nghe event với addEventListener

--- chapter06/video50/video50.js
+++ chapter06/video50/video50.js
@@ -2,8 +2,22 @@ console.log("video50");
 
 const element = document.getElementById("myBtn");
 
-console.log(element);
+// case1:
 
-function handleClickBtn() {
+// element.addEventListener("click", function () {
+//   console.log("you click a button");
+// });
+
+// case2:
+
+const handleClickBtn = () => {
   console.log("you click a button");
-}
+};
+
+element.addEventListener("click", handleClickBtn);
+
+// su khac nhau nho giua
+// 1) function(): luc nay fun phai thuc thi ngay lap tuc
+// 2) function: luc nay fun chi phai thuc thi khi co tac dong vao
+
+console.log(element);
