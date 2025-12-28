aeaed9087e2f0a6695defe8c6b857f6713657b3a
royhung <lemanhhung1555@gmail.com>
2025-12-28 22:48:00 +0700

    chua bt lab 05

---
+++ chapter07/lab05/lab05.js
@@ -0,0 +1,24 @@
+console.log("Lab05");
+
+const fetchUsers = async () => {
+  const res = await fetch("http://localhost:8000/users");
+  const data = await res.json();
+
+  console.log("check data: ", data);
+
+  //insert data to html
+  const tbody = document.querySelector("#users tbody");
+  if (data && data.length) {
+    data.forEach((user, index) => {
+      tbody.innerHTML += `
+        <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+            </tr>
+        `;
+    });
+  }
+};
+
+fetchUsers();
