36c9de6d5880c95b87a735de35bc08b290736d1e
royhung <lemanhhung1555@gmail.com>
2025-12-29 01:35:00 +0700

    chua bt thuc hanh chapter 08 - part 3

--- chapter08/video70.js
+++ chapter08/video70.js
@@ -40,17 +40,19 @@ const generateTodoTable = () => {
   const todoListStr = localStorage.getItem("todo");
   if (todoListStr) {
     const todoList = JSON.parse(todoListStr);
-    console.log(todoList);
 
     //insert data to html
     const tbody = document.querySelector("#todoList tbody");
     if (todoList && todoList.length) {
       todoList.forEach((todo, index) => {
         tbody.innerHTML += `
-        <tr>
+            <tr>
                 <td>${todo.id}</td>
                 <td>${todo.name}</td>
-                <td><button>Xoa</button></td>
+                
+                <td>
+                <button data-id=${todo.id} class="btn-delete" >Xoa</button>
+                </td>
             </tr>
         `;
       });
@@ -59,3 +61,26 @@ const generateTodoTable = () => {
 };
 
 generateTodoTable();
+
+const deleteBtns = document.querySelectorAll(".btn-delete");
+if (deleteBtns) {
+  deleteBtns.forEach((btn, index) => {
+    console.log(btn, index);
+    btn.addEventListener("click", () => {
+      const id = btn.getAttribute("data-id");
+      handleDeleteTodo(id);
+    });
+  });
+}
+
+const handleDeleteTodo = (id) => {
+  const todoListStr = localStorage.getItem("todo");
+  if (todoListStr) {
+    const todoList = JSON.parse(todoListStr);
+
+    const newTodo = todoList.filter((todo, index) => todo.id + "" !== id);
+    localStorage.setItem("todo", JSON.stringify(newTodo));
+
+    window.location.reload();
+  }
+};
