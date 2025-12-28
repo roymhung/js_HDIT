0dd9f6fada7e8c61fd7b8bba7f220da99e25d0be
royhung <lemanhhung1555@gmail.com>
2025-12-28 22:15:21 +0700

    async vs await

---
+++ chapter07/video66/video66.js
@@ -0,0 +1,13 @@
+console.log("video66");
+
+//promise
+fetch("http://localhost:8000/users")
+  .then((res) => res.json())
+  .then((data) => console.log("fetch promise", data));
+
+//async await
+const fetchData = async () => {
+  const res = await fetch("http://localhost:8000/users");
+  const data = res.json();
+  console.log(data);
+};
